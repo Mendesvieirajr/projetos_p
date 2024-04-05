97e0f8916b2c8393bb4136a631baeb2ffc927a39
Martim Vieira <ma.martimvieira@gmail.com>
2024-04-05 11:51:19 +0100

    Um tentativa em qpe o botão de pausa não altera o texto

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
+function donot(){
+    return !playbutton.classlistcontains('hidden');
+}
+
 function toggleButtons() {
     // Obter referências aos botões
     var playButton = document.getElementById('playButton');
     var stopButton = document.getElementById('stopButton');
+    var pauseButton = document.getElementById('pauseButton');
     var timerText = document.getElementById('timerText');
 
     // Alternar a visibilidade dos botões
     if (playButton.classList.contains('hidden')) {
         playButton.classList.remove('hidden');
         stopButton.classList.add('hidden');
-        timerText.classList.remove('hidden')
+        pauseButton.classList.remove('hidden');
+        timerText.classList.remove('hidden');
         timerText.textContent = "O timer parou às " + getCurrentTime() + " horas.";
-    } else {
+    }
+
+    else {
+        pauseButton.classList.remove('hidden');
         playButton.classList.add('hidden');
         stopButton.classList.remove('hidden');
-        timerText.classList.remove('hidden')
+        timerText.classList.remove('hidden');
         timerText.textContent = "O timer começou às " + getCurrentTime() + " horas.";
     }
+    
 }
 function toggleButtons2(){
-    // Referencias dos botões
+    var playButton = document.getElementById('playButton');
+    var stopButton = document.getElementById('stopButton');
     var pauseButton = document.getElementById('pauseButton');
-    var replayButton = document.getElementById('replayButton');
+    var timerText = document.getElementById('timerText');
 
     if(pauseButton.classList.contains('hidden')){
         pauseButton.classList.remove('hidden');
-        replayButton.classList.add('hidden');
-        timerText.classList.remove('hidden')
-        timerText.textContent = "O timer retomou às" + getCurrentTime() + "horas."
-    }
-    else {
+        playButton.classList.add('hidden');
+        timerText.classList.remove('hidden');
+        timerText.textContent = "O timer retomou às " + getCurrentTime() + " horas.";
+    } else {
         pauseButton.classList.add('hidden');
-        replayButton.classList.remove('hidden');
-        timerText.classList.remove('hidden')
-        timerText.textContent = "O timer pausou às" + getCurrentTime() + "horas.";
+        playButton.classList.remove('hidden');
+        timerText.classList.remove('hidden');
+        if (stopButton.classList.contains('hidden')) {
+            timerText.textContent = "O timer parou às " + getCurrentTime() + " horas.";
+        } else {
+            timerText.textContent = "O timer começou às " + getCurrentTime() + " horas.";
+        }
     }
 }
 
